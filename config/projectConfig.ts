adaf6d62e720a298ab213ec4a2e3c0e4e0fb2bfa
adikencana17 <103553495+adikencana17@users.noreply.github.com>
2022-04-13 13:54:33 +0800

    Update projectConfig.ts

--- config/projectConfig.ts
+++ config/projectConfig.ts
@@ -7,21 +7,21 @@ const projectConfig = {
 
   maxMintAmountPerTxn: 100,
 
-  mintCost: process.env.NODE_ENV === 'production' ? 100 : 0.01,
+  mintCost: process.env.NODE_ENV === 'development' ? 100 : 0.01,
 
   networkName:
-    process.env.NODE_ENV === 'production'
+    process.env.NODE_ENV === 'development'
       ? 'Ethereum Mainnet' // 'Ethereum Mainnet'
       : 'Rinkey Testnet', // 'Rinkeby Testnet'
 
   chainName: 'ETH', // 'ETH'
 
-  chainId: process.env.NODE_ENV === 'production' ? 1 : 4, // Ethereum (1), Rinkeby (4)
+  chainId: process.env.NODE_ENV === 'development' ? 1 : 4, // Ethereum (1), Rinkeby (4)
 
   siteDomain: 'www.pgrobots.com',
 
   siteUrl:
-    process.env.NODE_ENV === 'production'
+    process.env.NODE_ENV === 'development'
       ? `https://your_site_domain`
       : 'http://localhost:3000',
 
@@ -32,17 +32,17 @@ const projectConfig = {
   discordUrl: 'https://discord.gg/testest',
 
   openseaCollectionUrl:
-    process.env.NODE_ENV === 'production'
+    process.env.NODE_ENV === 'development'
       ? 'https://opensea.io/collection/your_opensea_collection_name'
       : 'https://testnets.opensea.io/collection/your_opensea_collection_name',
 
   contractAddress:
-    process.env.NODE_ENV === 'production'
+    process.env.NODE_ENV === 'development'
       ? 'your_mainnet_contract_address'
       : 'your_testnet_contract_address',
 
   scanUrl:
-    process.env.NODE_ENV === 'production'
+    process.env.NODE_ENV === 'development'
       ? 'https://etherscan.io/address/your_ethereum_contract_addres'
       : 'https://rinkeby.etherscan.io/address/0x9ebaca53634d3bcb29b3d6defbb32ae17d389014',
   // 'https://etherscan.io/address/your_ethereum_contract_address'
