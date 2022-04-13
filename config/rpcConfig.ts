f42cafbbedee6f981eab26f4e5eabc9aaa875e89
adikencana17 <103553495+adikencana17@users.noreply.github.com>
2022-04-13 14:34:49 +0800

    Update rpcConfig.ts

--- config/rpcConfig.ts
+++ config/rpcConfig.ts
@@ -1,6 +1,6 @@
 // For Ethereum, use the Infura endpoints
 export default function rpcConfig(infuraKey?: string) {
-  return process.env.NODE_ENV === 'production'
+  return process.env.NODE_ENV === 'development'
     ? 'https://mainnet.infura.io/v3/cd5f677e2ff24d96861c383e8ad77082' // `https://mainnet.infura.io/v3/${infuraKey}`
     : 'https://rinkeby.infura.io/v3/cd5f677e2ff24d96861c383e8ad77082'; // `https://rinkeby.infura.io/v3/${infuraKey}`
 }
