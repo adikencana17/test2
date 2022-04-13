757ce8b75f5f67f9ab73cbcdf807ffe8f6dafbb7
adikencana17 <103553495+adikencana17@users.noreply.github.com>
2022-04-13 14:52:59 +0800

    Update Roadmap.tsx

--- components/Roadmap.tsx
+++ components/Roadmap.tsx
@@ -4,22 +4,10 @@ export default function Roadmap() {
       <h2 className="text-4xl mb-4">Roadmap</h2>
       <ul className="list-disc pl-5 space-y-2 mb-4">
         <li>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
+        No roadmap just good vibes!
         </li>
         <li>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
+         Oh yeah!
         </li>
       </ul>
     </>
